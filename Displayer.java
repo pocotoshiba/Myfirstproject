b8329878c2c1453d881f81a6cd84c298173c9acc
pocotoshiba <Benjava93@outlook.com>
2025-01-31 16:36:20 +0000

    Add files via upload

---
+++ Displayer.java
@@ -0,0 +1,11 @@
+public class Displayer // class name should be the same as the file name
+{ // start of the class
+    
+    public static void main(String [] args) // main method is the starting point of the program
+    { // start of the main method
+        System.out.println("Hello World"); 
+        System.out.println("This is my first project"); // it is used to print the output in the next line
+        System.out.print("I am learning Java"); // I use this to print the output in the same line
+    } // end of the main method
+} // end of the class
+/* Press f11 to run the code  */
